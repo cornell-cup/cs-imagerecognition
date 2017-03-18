72b26af5ac6193c7e1b6c9f3a5fa89c95211329e
Jeffery Lee <jyl58@cornell.edu>
2017-03-18 13:54:41 -0400

    Fixed bugs, actually evaluates the appropriate image now

--- PiScripts/identifyFromCamera.sh
+++ PiScripts/identifyFromCamera.sh
@@ -3,8 +3,12 @@
 #Script that takes photo from Raspberry Pi Camera and runs image recognition on the image. The resulting classifications are stored in IMAGE_CLASS.txt
 
 #Assumes that takePicture.py and ./image_recog are in the SAME directory
+#Also assumes that takePicture.py stores the image in the same directory
 
+echo "Capuring image..."
 python takePicture.py
+echo "Identifying image..."
+path="$(pwd)/image.jpg"
+./image_recog_pi $path
 
-./image_recog_pi /home/pi/image.jpg
-
+rm image.jpg
