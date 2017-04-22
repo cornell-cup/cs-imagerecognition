dc7cd8aec5417f0a5c4190337ef9f87e58a9adc1
Jeffery Lee <jyl58@cornell.edu>
2017-04-22 12:54:30 -0400

    Sending/receiving images

--- PiScripts/receiveImage.py
+++ PiScripts/receiveImage.py
@@ -1,13 +1,16 @@
-# File to receive image data from the Pi
+# Script to receive image data from the Pi
 
 import socket
 import R2Protocol
 
-TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE= 1024
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#TCP_IP = s.getfqdn()
+#TCP_IP = '127.0.0.1'
+TCP_IP = ''
+
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
--- PiScripts/sendImage.py
+++ PiScripts/sendImage.py
@@ -6,24 +6,28 @@ import socket
 from PIL import Image
 
 #TCP protocol 
-TCP_IP = '127.0.0.1'
+#TCP_IP = '127.0.0.1'
+TCP_IP = '192.168.4.123'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 
 
-#Getting image
-stream = io.BytesIO()
-with picamera.PiCamera() as camera:
-	camera.capture(stream, format='jpeg')
-stream.seek(0)
-image = stream.getvalue()
-
-
-encoded = R2Protocol.encode("Pi", "NUC", "ID", image)
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-s.connect((TCP_IP, TCP_PORT))
-s.send(encoded)
-s.close()
+while(True):
+	#Getting image
+	stream = io.BytesIO()
+	with picamera.PiCamera() as camera:
+		camera.capture(stream, format='jpeg')
+	stream.seek(0)
+	image = stream.getvalue()
+	
+	encoded = R2Protocol.encode("Pi", "NUC", "ID", image)
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+		#print(s.__closed)
+		s.connect((TCP_IP, TCP_PORT))
+		s.send(encoded)
+		s.close()
+	except ConnectionRefusedError:
+		print("Connection not open")
 
 
