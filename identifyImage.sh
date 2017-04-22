ac4c1f6a11ea5a0ddc4d8d0150bc599133bef3a2
Jeffery Lee <jyl58@cornell.edu>
2017-04-22 13:40:48 -0400

    Script to receive and classify image

--- identifyImage.sh
+++ identifyImage.sh
@@ -2,18 +2,16 @@
 
 #Script that runs image recognition on a given image
 
-cd "C:\Users\CornellCup\models\tutorials\image\imagenet"
-
 source activate tensorflow
 
-if [ $# -ge 1 ]; then
-	#python classify_image.py --image_file=$1 2>"C:\Users\CornellCup\Desktop\image_recog.log 1>"C:\Users\CornellCup\Desktop\IMAGE_CLASS.txt"
-	time python classify_image.py --image_file=$1 2>"C:\Users\CornellCup\Documents\image-recog\image_recog.log" 1>"C:\Users\CornellCup\Documents\image-recog\IMAGE_CLASS.txt"
-else
-	#python classify_image.py 2>"C:\Users\CornellCup\Desktop\image_recog.log" 1>"C:\Users\CornellCup\Desktop\IMAGE_CLASS.txt"
-	time python classify_image.py 2>"C:\Users\CornellCup\Documents\image-recog\image_recog.log" 1>"C:\Users\CornellCup\Documents\image-recog\IMAGE_CLASS.txt"
+cd "C:\Users\CornellCup\Documents\image-recog\cs-object-identification"
 
-fi
+python PiScripts\receiveImage.py
+
+cd "C:\Users\CornellCup\models\tutorials\image\imagenet"
+
+#python classify_image.py --image_file=$1 2>"C:\Users\CornellCup\Desktop\image_recog.log 1>"C:\Users\CornellCup\Desktop\IMAGE_CLASS.txt"
+python classify_image.py --image_file="C:\Users\CornellCup\Documents\image-recog\cs-object-identification\image.jpeg" 2>"C:\Users\CornellCup\Documents\image-recog\image_recog.log" 1>"C:\Users\CornellCup\Documents\image-recog\IMAGE_CLASS.txt"
 
 source deactivate tensorflow
 
